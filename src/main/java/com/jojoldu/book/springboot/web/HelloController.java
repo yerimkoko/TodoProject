6bd424451090b19c7af51d8095b1852d97f30d62
yerimkoko <gochi97@naver.com>
2021-05-03 17:06:45 +0900

    study: 테스트 코드 작성해보기

--- src/main/java/com/jojoldu/book/springboot/web/HelloController.java
+++ src/main/java/com/jojoldu/book/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.jojoldu.book.springboot.web;
 
+import com.jojoldu.book.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController //json을 반환해준다.
@@ -9,4 +11,14 @@ public class HelloController {
     public String hello() {
         return "hello";
     }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
+
+    }
 }
+
+// @RequestParam
+// 외부에서 API로 넘긴 파라미터를 가져오는 어노테이션.
